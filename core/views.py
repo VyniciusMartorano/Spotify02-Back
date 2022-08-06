e7f1d1c5751135cfeaf31cbbe6328ff6a254e5ad
VYNICIUS MARTORANO <VyniciusMartorano>
2022-08-06 11:45:17 -0300

    last

--- core/views.py
+++ core/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import *
 from .serializers import *
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class MusicsViewSet(ModelViewSet):
     queryset = Musics.objects.all()
     serializer_class = MusicsSerializer
+
+    @action(methods=['post'], detail=False)
+    def retrieve_musics(self, ):
+        qs = Musics.objects.all()
+
+        return Response(request)
