6eb4c7b3cfd8fd841ebee33e627757d796a9eca8
VYNICIUS MARTORANO <VyniciusMartorano>
2022-08-06 10:21:20 -0300

    update

--- api/settings.py
+++ api/settings.py
@@ -113,6 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
--- core/models.py
+++ core/models.py
@@ -6,12 +6,12 @@ class Musics(models.Model):
     music_name = models.CharField(max_length=255, null=False, blank=False)
     artist = models.CharField(max_length=255, null=False, blank=False)
     genero = models.CharField(max_length=100, null=False, blank=False)
-    path = models.CharField(max_length=255, null=False, blank=False)
-    music_image = models.ImageField()
+    file = models.FileField(upload_to='', null=True)
+    image = models.ImageField()
     duration = models.FloatField(null=False, blank=False)
     liked = models.BooleanField(null=False, blank=False)
 
-    def __str__(self): return music_name
+    def __str__(self): return self.music_name
 
     class Meta:
         managed = False
